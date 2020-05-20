244cd107dd11b873d617dd0cf787641dd8383cf3
BandtitoA455 <karlis.oglins@gmail.com>
2020-05-20 21:36:01 +0300

    starting to make the side panel in welcome.php

---
+++ public/js/javascript.js
@@ -0,0 +1,7 @@
+function sidenav() {
+    document.getElementById("sidenav").style.width = '250px';
+}
+
+function sidenav_close(){
+    document.getElementById("sidenav").style.width = '0';
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
         {{-- <link rel="stylesheet" href="css/index.css" /> --}}
         <link rel="stylesheet" type="text/css" href="{{ asset('/css/styles.css') }}">
         <link rel="stylesheet" type="text/css" href="{{ asset('/css/reset.css') }}">
+        <script type="text/javascript" src="{{ URL::asset('js/javascript.js') }}"></script>
         <title>Laravel</title>
 
     </head>
@@ -15,13 +16,24 @@
         </div>
         <div>
             <ul>
-                <li class="float_left"><img class="logo" src="/images/logo.png"></li>
+                <li class="float_left"><img id="logo" onclick="sidenav()" class="logo" src="/images/logo.png"></li>
                 <li class="float_left"><a href="#" class="navbar_item">Home</a></li>
                 <li class="float_left"><a href="#" class="navbar_item">Support</a></li>
                 <li class="float_left"><a href="#" class="navbar_item">About</a></li>
                 <li class="float_right"><a href="#" class="navbar_item">Register</a></li>
                 <li class="float_right"><a href="#" class="navbar_item">Log in</a></li>
               </ul>
+        </div>
+        <div id="sidenav" class="sidenav">
+            <a href="javascript:void(0)" class="closebtn" onclick="sidenav_close()">&times;</a>
+            <a href="#spain">Spain</a>
+            <a href="#france">France</a>
+            <a href="#greece">Greece</a>
+            <a href="#china">China</a>
+            <a href="#japan">Japan</a>
+            <a href="#taiwan">Taiwan</a>
+            <a href="#form">Application form</a>
+            <a href="#information">About Us</a>
         </div>
     </body>
 </html>
