11c4a479e7a80b003318f078fd76498a00cc1d68
reinispope <reinis.pope@gmail.com>
2020-05-20 21:35:58 +0300

    added product seeder

---
+++ database/seeds/ProductSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('products')->insert([
+            'name' => Str::random(10),
+            'type' => Str::random(10),
+            'price' => '10.55',
+            ]);
+    }
+}
