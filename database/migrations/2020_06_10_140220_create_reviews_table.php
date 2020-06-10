45905f63501329e6bd045c9c03a6c68e404c7642
reinispope <reinis.pope@gmail.com>
2020-06-10 17:26:16 +0300

    added migrations

---
+++ database/migrations/2020_06_10_140220_create_reviews_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+
+            $table->bigIncrements('reviewID');
+            $table->foreign('userID')->references('id')->on('users');
+            $table->string('review');
+            $table->decimal('rating', 38, 2);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
