e1024671b5309bb6ad4899683045c51baa360efe
BandtitoA455 <karlis.oglins@gmail.com>
2020-06-10 13:41:30 +0300

    changes something

---
+++ resources/views/layouts/app1.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        {{-- <link rel="stylesheet" type="text/css" href="{{ asset('/css/styles.css') }}"> --}}
+        <link href="{{ asset("css/template.css") }}" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="{{ asset('/css/reset.css') }}">
+        <script type="text/javascript" src="{{ URL::asset('js/javascript.js') }}"></script>
+        @yield('HeadContent')
+
+        <title>Laravel</title>
+    </head>
+    <body>
+        <div class="header">
+            <h1><strong>Melnā poga</strong></h1>
+        </div>
+        <div>
+            <ul>
+                <li class="float_left"><img id="logo" onclick="sidenav()" class="logo" src="/images/logo.png"></li>
+                <li class="float_left"><a href="{{ url('/') }}" class="navbar_item">Home</a></li>
+                <li class="float_left"><a href="{{ url('/support') }}" class="navbar_item">Support</a></li>
+                <li class="float_left"><a href="{{ url('/about') }}" class="navbar_item">About</a></li>
+                <li class="float_right"><a href="{{ url('/register') }}" class="navbar_item">Register</a></li>
+                <li class="float_right"><a href="{{ url('/login') }}" class="navbar_item">Log in</a></li>
+              </ul>
+        </div>
+
+        <div id="sidenav" class="sidenav">
+            <a href="javascript:void(0)" class="closebtn" onclick="sidenav_close()">&times;</a> 
+            @yield('SideNavContent') 
+        </div>
+
+        @yield('BodyContent')
+
+    </body>
