a275677d7697231927b0543e5fd46394016def95
BandtitoA455 <karlis.oglins@gmail.com>
2020-06-12 15:50:30 +0300

    did nothing

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -14,16 +14,35 @@
             <h1><strong>Melnā poga</strong></h1>
         </div>
         <div>
-            <ul>
+            <ul class="navback">
                 <li class="float_left"><img id="logo" onclick="sidenav()" class="logo" src="/images/logo.png"></li>
                 <li class="float_left"><a href="{{ url('/') }}" class="navbar_item">Home</a></li>
                 <li class="float_left"><a href="{{ url('/support') }}" class="navbar_item">Support</a></li>
                 <li class="float_left"><a href="{{ url('/about') }}" class="navbar_item">About</a></li>
-                <li class="float_left"><a href="{{ route('admin.users.index') }}" class="navbar_item">Users</a></li>
+                
+                @can('manage-users')
+                    <li class="float_left"><a href="{{ route('admin.users.index') }}" class="navbar_item">Users</a></li>
+                @endcan
 
-                <li class="float_right"><a href="{{ url('/register') }}" class="navbar_item">Register</a></li>
-                <li class="float_right"><a href="{{ url('/login') }}" class="navbar_item">Log in</a></li>
-              </ul>
+                @guest
+                
+                
+                @if (Route::has('register'))
+                    <li class="float_right"><a href="{{ route('register') }}" class="navbar_item">Register</a></li>
+                    <li class="float_right"><a href="{{ route('login') }}" class="navbar_item">Log in</a></li>
+                @endif
+                
+                @else
+                    <li class="float_right">
+                        <a class="navbar_item" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;"> @csrf </form>
+                    </li>
+                @endguest
+            </ul>
         </div>
 
         @yield('BodyContent')
