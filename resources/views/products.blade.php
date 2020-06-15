4ee57d5ca69a703adf2fb60b54bd1b3acf00a9c1
reinispope <reinis.pope@gmail.com>
2020-06-15 16:41:26 +0300

    finalized filters

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -4,24 +4,7 @@
 
     <h1>product page</h1>
     <table>
-    @foreach($prods as $data)
-    <tr >
-        <td><img src={{asset('images/'.$data->id.'.jpg')}}></td>
-      <td style="border: 1px solid black">{{$data->productname}}</td>
-      <td style="border: 1px solid black">{{$data->productprice}}</td>
-      <td style="border: 1px solid black">{{$data->producttype}}</td>
-      <td><img src={{asset('images/'.$data->id.'.jpg')}}></td>
-    </tr>
-    @endforeach
-</table>
 
-        <select class="form-control" name="lastname" id="lastname" data-parsley-required="true">
-          @foreach ($types as $types)
-          {
-            <option value="{{ $types }}">{{ $types }}</option>
-          }
-          @endforeach
-        </select>
 </div>
 
 </body>
